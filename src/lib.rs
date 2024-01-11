180740cf08ffc2dfd5899412efd2f1ad876a9b67
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2024-01-12 00:31:13 +0100

    Add CloseHandle function for Windows

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use Result::Err;
 
 #[cfg(target_os = "windows")]
 use windows::Win32::{
-    Foundation::{HANDLE, GetLastError},
+    Foundation::{HANDLE, GetLastError, CloseHandle},
     System::Threading::{OpenProcess, OpenProcessToken, PROCESS_QUERY_LIMITED_INFORMATION},
     Security::{GetTokenInformation, TokenElevation, TOKEN_ELEVATION, TOKEN_QUERY}
 };
@@ -14,11 +14,21 @@ use std::mem::size_of;
 #[cfg(target_os = "windows")]
 use std::ffi::c_void;
 
-#[cfg(any(target_os = "linux", target_os = "macos"))]
+#[cfg(target_os = "windows")]
+struct SafeHandle(HANDLE);
+
+#[cfg(target_os = "windows")]
+impl Drop for SafeHandle {
+    fn drop(&mut self) {
+        unsafe { let _ = CloseHandle(self.0); };
+    }
+}
+
+#[cfg(target_os = "linux")]
 use nix::unistd::{Pid, Uid};
-#[cfg(any(target_os = "linux", target_os = "macos"))]
+#[cfg(target_os = "linux")]
 use nix::libc::getuid;
-#[cfg(any(target_os = "linux", target_os = "macos"))]
+#[cfg(target_os = "linux")]
 use procfs::process::Process;
 
 #[napi]
@@ -26,22 +36,22 @@ pub fn is_elevated(pid: u32) -> Result<bool, Error> {
 
     #[cfg(target_os = "windows")]
     unsafe {
-        let process_handle = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, false, pid);
 
-        if process_handle.is_err() {
-            return Err(Error::from_reason(format!("OpenProcess failed with error code: {:?}", GetLastError())));
-        }
+        let process_handle = match OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, false, pid) {
+            Ok(handle) => SafeHandle(handle),
+            Err(_) => return Err(Error::from_reason(format!("OpenProcess failed with error code: {:?}", GetLastError())))
+        };
 
-        let mut token_handle: HANDLE = HANDLE::default();
+        let mut token_handle: SafeHandle = SafeHandle(HANDLE::default());
 
-        if OpenProcessToken(process_handle.unwrap(), TOKEN_QUERY, &mut token_handle).is_err() {
+        if OpenProcessToken(process_handle.0, TOKEN_QUERY, &mut token_handle.0).is_err() {
             return Err(Error::from_reason(format!("OpenProcessToken failed with error code: {:?}", GetLastError())));
         }
 
         let mut elevation = TOKEN_ELEVATION::default();
         let mut size = size_of::<TOKEN_ELEVATION>() as u32;
 
-        if GetTokenInformation(token_handle, TokenElevation, Some(&mut elevation as *mut _ as *mut c_void), size, &mut size).is_err() {
+        if GetTokenInformation(token_handle.0, TokenElevation, Some(&mut elevation as *mut _ as *mut c_void), size, &mut size).is_err() {
             return Err(Error::from_reason(format!("GetProcessInformation failed with error code: {:?}", GetLastError())));
         }
 
@@ -49,14 +59,17 @@ pub fn is_elevated(pid: u32) -> Result<bool, Error> {
 
     }
 
-    #[cfg(any(target_os = "linux"))]
+    #[cfg(target_os = "linux")]
     {
-        let process = Process::new(pid as i32).map_err(|_| Error::from_reason("Unable to build process from pid"))?;
+        let process = match Process::new(pid as i32) {
+            Ok(proc) => proc,
+            Err(e) => return Err(Error::from_reason(format!("Unable to build process from pid: {}", e)))
+        };
 
-        match process.uid() {
+        return match process.uid() {
             Ok(uid) => Ok(Uid::is_root(Uid::from_raw(uid))),
-            Err(e) => Err(Error::from_reason("Unable to get process uid"))
-        }
+            Err(e) => Err(Error::from_reason(format!("Unable to get process uid: {}", e)))
+        };
     }
 
     #[cfg(not(any(target_os = "windows", target_os = "linux")))]
